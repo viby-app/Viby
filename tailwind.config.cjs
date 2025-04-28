f31c644e0e10ba2bb64080cf28f2b2faeccf5e83
Levi Fichman <61022686+LTheCoder@users.noreply.github.com>
2025-04-28 10:21:09 +0300

    added homepage and menu ui needs fixing

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#F2EFE7",
+        accentLight: "#9ACBD0",
+        accent: "#48A6A7",
+        primary: "#006A71",
+        text: "#222222",
+      },
+    },
+  },
+  plugins: [],
+};
