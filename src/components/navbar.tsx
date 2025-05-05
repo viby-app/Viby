00d9cef5f6f8c1b736c4ba38dcfe4a0655d3ad3d
Levi Fichman <61022686+LTheCoder@users.noreply.github.com>
2025-05-05 14:48:37 +0300

    removed log out button from menu (not supposed to be in there)

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,10 +4,9 @@ import { signOut } from "next-auth/react";
 
 const Navbar = () => {
   return (
-    <nav className="fixed bottom-0 z-50 flex w-full items-center justify-around border-t border-gray-800 bg-[#48A6A7] p-4 py-2 lg:top-0 lg:max-h-10">
+    <nav className="fixed bottom-0 z-50 flex w-full items-center justify-around border-t border-gray-800 bg-[#48A6A7] p-5 py-2 lg:top-0 lg:max-h-10">
       <div className="container mx-auto flex items-center justify-between">
         <ul className="flex space-x-4">
-          <Button onClick={async () => await signOut()}>log out</Button>
           {pages.map((page) => (
             <li key={page.name} className="min-w-10">
               <a href={page.path} className="text-white hover:text-gray-200">
