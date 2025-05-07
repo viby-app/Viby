590307879b1b7a3fa9e920298e80e0cd60c4e86b
viby-app <viby1.app@gmail.com>
2025-05-07 09:46:25 +0300

    Arc 4/edit prisma schema (#27)
    * prisma schema A
    
    * add vercel build script
    
    * create business route
    
    * remove post
    
    * add isPhoneVerified to prisma schema User model
    
    * changed isPhoneVerified to required
    
    * added migration
    
    ---------
    
    Co-authored-by: ofek_david <ofekdavid105@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
-import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
 
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.post.hello.useQuery({ text: "from tRPC" });
+  const hello = api.business.hello.useQuery({ text: "from tRPC" });
 
   return (
     <>
@@ -47,7 +46,6 @@ export default function Home() {
             <p className="text-2xl text-white">
               {hello.data ? hello.data.greeting : "Loading tRPC query..."}
             </p>
-            <AuthShowcase />
           </div>
         </div>
       </main>
@@ -55,26 +53,3 @@ export default function Home() {
   );
 }
 
-function AuthShowcase() {
-  const { data: sessionData } = useSession();
-
-  const { data: secretMessage } = api.post.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined }
-  );
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn("google")}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
-  );
-}
---
+++ src/server/api/routers/business.ts
@@ -0,0 +1,25 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
+
+export const businessRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
+
+  getLatest: protectedProcedure.query(async ({ ctx }) => {
+    const business = await ctx.db.business.findFirst({
+      orderBy: { createdAt: "desc" },
+    });
+
+    return business ?? null;
+  }),
+});
