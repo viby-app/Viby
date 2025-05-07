00eb2379fcdb84fbc57ae2c4d94bd9253814f3e1
Levi Fichman <61022686+LTheCoder@users.noreply.github.com>
2025-05-07 13:17:28 +0300

    fix: downgrade version of next-auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,4 @@
 import NextAuth from "next-auth";
 import { authOptions } from "~/server/auth/config";
-const handlers = NextAuth(authOptions);
 
-export { handlers as GET, handlers as POST };
+export default NextAuth(authOptions);
