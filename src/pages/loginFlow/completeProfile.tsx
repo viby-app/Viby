04629022c824a078714ed2b92aed05edb063cb85
Levi Fichman <61022686+LTheCoder@users.noreply.github.com>
2025-06-24 17:58:21 +0300

    fix: remove disabled option from gender select in complete profile form

--- src/pages/loginFlow/completeProfile.tsx
+++ src/pages/loginFlow/completeProfile.tsx
@@ -101,9 +101,6 @@ export default function CompleteProfileForm() {
               {...register("gender")}
               className="mt-1 w-full rounded-md bg-white p-2 text-[#3A3A3A] shadow-md"
             >
-              <option disabled>
-                {hebrewDictionary.chooseGender}
-              </option>
               <option value="MALE">{hebrewDictionary.male}</option>
               <option value="FEMALE">{hebrewDictionary.female}</option>
               <option value="OTHER">{hebrewDictionary.other}</option>
