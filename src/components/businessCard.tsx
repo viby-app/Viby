842db5bf85ad270bfa340eae4aea772fe52b8b45
Levi Fichman <61022686+LTheCoder@users.noreply.github.com>
2025-08-10 21:01:58 +0300

    fix: adjust layout and formatting in BusinessCard component for better readability

--- src/components/businessCard.tsx
+++ src/components/businessCard.tsx
@@ -55,7 +55,10 @@ const BusinessCard: React.FC<Props> = ({ businessId }) => {
 
   return (
     <Link href={businessRoute(businessId)}>
-      <div className="flex h-full max-h-[400px] w-full min-w-3xs flex-col rounded-2xl border-4 border-[#48a5a748]">
+      <div
+        dir="rtl"
+        className="flex h-full max-h-[400px] w-full min-w-3xs flex-col rounded-2xl border-4 border-[#48a5a748]"
+      >
         <div className="rounded-t-xl px-4 py-2 text-center">
           <div className="flex flex-row items-center justify-start gap-2">
             {logoLoading ? (
@@ -70,7 +73,9 @@ const BusinessCard: React.FC<Props> = ({ businessId }) => {
                 height={40}
                 className="h-10 w-10 rounded-full border object-cover"
               />
-            ) : (<BriefcaseBusinessIcon />)}
+            ) : (
+              <BriefcaseBusinessIcon />
+            )}
             <div className="text-right">
               <h2 className="truncate text-lg font-bold">{business?.name}</h2>
               <p className="truncate text-sm text-gray-700">
